fix loglikelihood

IMLearn/learners/gaussian_estimators.py
@@ -115,10 +115,9 @@ class UnivariateGaussian:
         """
 
         """based on question 13"""
-        d = len(X)
-        first_arg = (-mu * d * numpy.log(2 * numpy.pi)) / 2
-        second_arg = (-mu / 2) * numpy.log(sigma)
-        third_arg = (-1 / 2) * numpy.sum((X - mu) * (1 / sigma) * (X - mu))
+        first_arg = -(np.log(2 * np.pi)) * len(X) / 2
+        second_arg = (- len(X) / 2) * np.log(sigma)
+        third_arg = (-1 / (2 * sigma)) * np.sum((X - mu) ** 2)
         log_likelihood = first_arg + second_arg + third_arg
         return log_likelihood
 
@@ -169,8 +168,8 @@ class MultivariateGaussian:
         """
 
         self.mu_ = X.mean(axis=0)
-        x_hat = X-self.mu_
-        self.cov_ = np.multiply(x_hat.T, x_hat) * (1 / len(X))
+        x_hat = X - self.mu_
+        self.cov_ = np.matmul(x_hat.T, x_hat) / (len(X) - 1)
         self.fitted_ = True
         return self
 
@@ -195,7 +194,18 @@ class MultivariateGaussian:
         if not self.fitted_:
             raise ValueError(
                 "Estimator must first be fitted before calling `pdf` function")
-        raise NotImplementedError()
+        shape = np.shape(X)
+        n_samples = shape[0]
+        d = shape[1]
+        sigma_inverse = np.linalg.inv(self.cov_)
+        sigma_det = det(self.cov_)
+        pdf = np.zeros(n_samples)
+        first_arg = 1 / np.sqrt(np.power(2 * np.pi, d) * sigma_det)
+        for i in range(n_samples):
+            xi_mu = X[i] - self.mu_
+            exp_i = numpy.exp(-0.5 * (xi_mu.T @ sigma_inverse @ xi_mu))
+            pdf[i] = first_arg * exp_i
+        return pdf
 
     @staticmethod
     def log_likelihood(mu: np.ndarray, cov: np.ndarray,
@@ -217,4 +227,14 @@ class MultivariateGaussian:
         log_likelihood: float
             log-likelihood calculated over all input data and under given parameters of Gaussian
         """
-        raise NotImplementedError()
+        shape = np.shape(X)
+        m = shape[0]
+        d = shape[1]
+        xi_mu = X - mu
+        sigma_inverse = np.linalg.inv(cov)
+        (sign, log_det) = slogdet(cov)
+        first_arg = -0.5 * m * d * np.log(2 * np.pi)
+        second_arg = -0.5 * m * log_det
+        third_arg = -.5 * (np.sum(xi_mu @ sigma_inverse * xi_mu))
+        result = first_arg + second_arg + third_arg
+        return result

exercises/fit_gaussian_estimators.py
@@ -1,8 +1,10 @@
 import numpy as np
+import numpy.random
 import plotly.express as px
 import plotly.io as pio
+import plotly.graph_objects as go
 
-from IMLearn.learners import UnivariateGaussian
+from IMLearn.learners import UnivariateGaussian, MultivariateGaussian
 
 pio.templates.default = "simple_white"
 
@@ -26,7 +28,8 @@ def test_univariate_gaussian():
                            "x": "Sample Size",
                            "y": "Distance from mu"
                        },
-                       title="Sample mean estimation deviation",
+                       title="Distance Between Estimated And True "
+                             "Value Of The Expectation",
                        )
     fig_2.write_image("q2.result.png")
 
@@ -39,23 +42,52 @@ def test_univariate_gaussian():
                            "x": "x",
                            "y": "PDF"
                        },
-                       title="Empirical PDF",
+                       title="PDF Of The Previous Drawn Samples",
                        )
     fig_3.write_image("q3.result.png")
 
 
 def test_multivariate_gaussian():
     # Question 4 - Draw samples and print fitted model
-    raise NotImplementedError()
+    mu = [0, 0, 4, 0]
+    cov = np.array(
+        [[1, 0.2, 0, 0.5], [0.2, 2, 0, 0], [0, 0, 1, 0], [0.5, 0, 0, 1]])
+    samples = numpy.random.multivariate_normal(mu, cov, 1000)
+    mg = MultivariateGaussian()
+    mg.fit(samples)
+    print(mg.mu_)
+    print(mg.cov_)
 
-    # Question 5 - Likelihood evaluation
-    raise NotImplementedError()
+    # # Question 5 - Likelihood evaluation
+    linspace = np.linspace(-10, 10, 200)
+    log_likelihood_result = np.zeros((200, 200))
+    for i in range(len(linspace)):
+        for j in range(len(linspace)):
+            mu = np.array([linspace[i], 0, linspace[j], 0]).T
+            likelihood_result = MultivariateGaussian. \
+                log_likelihood(mu, cov, samples)
+            log_likelihood_result[i][j] = likelihood_result
 
-    # Question 6 - Maximum likelihood
-    raise NotImplementedError()
+    fig_5 = go.Figure(
+        [go.Heatmap(x=linspace, y=linspace, z=log_likelihood_result, )],
+    )
+    fig_5.update_layout(
+        title='Log-Likelihood Where Mu Is [f1,0,f3,0]',
+        xaxis_title="$\\text{f3}$",
+        yaxis_title="$\\text{f1}$"
+    )
+    fig_5.write_image("q5.result.png")
+
+    # # Question 6 - Maximum likelihood
+    max_index = np.argmax(log_likelihood_result)
+    max_index_row = int(np.floor(max_index / 200))
+    max_index_col = (max_index - max_index_row * 200) % 200
+    print("f1: " + str(round(linspace[max_index_row], 3)))
+    print("f3: " + str(round(linspace[max_index_col], 3)))
+    print(log_likelihood_result[max_index_row][max_index_col])
 
 
 if __name__ == '__main__':
     np.random.seed(0)
     test_univariate_gaussian()
-    # test_multivariate_gaussian()
+    test_multivariate_gaussian()